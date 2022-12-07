2290f6da506fb68a862094f8e76294ed559d40c8
LuizFelipeDosSantosPaiva <luizfelipe484@outlook.com>
2022-12-07 19:24:28 -0300

    5

---
+++ cinema.js
@@ -0,0 +1,6 @@
+const catalogo = require("./databas/catalogo.json")
+console.log(catalogo)
+console.log(catalogo)
+// listar todos os filmes em cartaz 
+// achar um filme pelo codigo dele - parametro =codigo 
+// alterar o status do filme de false pra true e vise e versa 
